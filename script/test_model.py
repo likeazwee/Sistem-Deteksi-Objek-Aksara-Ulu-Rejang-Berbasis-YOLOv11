f5315a1567dacb774c8095c162f970a55cbaa829
Agyl Wendi Pratama <145989523+likeazwee@users.noreply.github.com>
2026-05-07 21:25:22 +0700

    Update test_model.py

--- script/test_model.py
+++ script/test_model.py
@@ -2,5 +2,5 @@ from ultralytics import YOLO
 
 model = YOLO("models/best.pt")
 
-gambar_tes = r"test/images/3_jpg.rf.09612c2902eaf8ac9193bff9abd81b3c.jpg"
+gambar_tes = r"gambar_dari_dataset"
 results = model.predict(source=gambar_tes, show=True, save=True)
